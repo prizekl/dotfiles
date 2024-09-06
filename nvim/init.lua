066f0ac2786bf0712ce2853ca6ab3fe6246bea80
prizelk <19prizel@gmail.com>
2024-09-06 09:45:33 -0700

    autopairs leaner

--- nvim/init.lua
+++ nvim/init.lua
@@ -246,16 +246,7 @@ require('lazy').setup({
     end,
   },
 
-  {
-    'windwp/nvim-autopairs',
-    dependencies = { 'hrsh7th/nvim-cmp' },
-    config = function()
-      require('nvim-autopairs').setup {}
-      local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
-      local cmp = require 'cmp'
-      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
-    end,
-  },
+  { 'windwp/nvim-autopairs', opts = {} },
 
   {
     'nvim-telescope/telescope.nvim',
