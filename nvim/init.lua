8d3d4730d1c8088f7668bbc02031fcb1f6091db8
prizelk <19prizel@gmail.com>
2024-09-21 23:15:54 -0700

    revert

--- nvim/init.lua
+++ nvim/init.lua
@@ -438,8 +438,8 @@ require('lazy').setup({
   {
     'nvim-lualine/lualine.nvim',
     config = function()
-      vim.api.nvim_set_hl(0, 'StatusLineNC', { bg = 'NvimLightGrey2' })
-      vim.api.nvim_set_hl(0, 'StatusLine', { bg = 'NvimLightGrey2' })
+      vim.api.nvim_set_hl(0, 'StatusLine', { bg = 'NvimDarkGrey3' })
+      vim.api.nvim_set_hl(0, 'StatusLineNC', { bg = 'NvimDarkGrey2' })
 
       local lualine_theme = {
         normal = {
@@ -448,9 +448,9 @@ require('lazy').setup({
           c = { bg = 'NvimDarkGrey3', fg = 'NvimLightGrey2' },
         },
         inactive = {
-          a = { bg = 'NvimDarkGrey3', fg = 'NvimLightGrey2', gui = 'bold' },
-          b = { bg = 'NvimDarkGrey3', fg = 'NvimLightGrey2' },
-          c = { bg = 'NvimDarkGrey3', fg = 'NvimLightGrey2' },
+          a = { bg = 'NvimDarkGrey2', fg = 'NvimLightGrey2', gui = 'bold' },
+          b = { bg = 'NvimDarkGrey2', fg = 'NvimLightGrey2' },
+          c = { bg = 'NvimDarkGrey2', fg = 'NvimLightGrey2' },
         },
       }
 
