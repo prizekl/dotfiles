2dd0b068881b19dd63afc5a73d36a128a8625354
prizelk <19prizel@gmail.com>
2024-09-27 10:22:43 -0700

    treesj

--- nvim/init.lua
+++ nvim/init.lua
@@ -339,7 +339,10 @@ require('lazy').setup({
             use_default_keymaps = false,
           }
 
-          vim.keymap.set('n', '<leader>m', ':TSJToggle<CR>')
+          vim.keymap.set('n', '<leader>m', require('treesj').toggle)
+          vim.keymap.set('n', '<leader>M', function()
+            require('treesj').toggle { split = { recursive = true } }
+          end)
         end,
       },
       {
