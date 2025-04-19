aa31a658a728239733fcf98adba15a05f08be9ef
prizelk <19prizel@gmail.com>
2025-04-19 00:39:11 -0700

    a

--- nvim/init.lua
+++ nvim/init.lua
@@ -91,27 +91,21 @@ require('lazy').setup({
       on_attach = function(bufnr)
         local gitsigns = require 'gitsigns'
 
-        local function map(mode, l, r, opts)
-          opts = opts or {}
-          opts.buffer = bufnr
-          vim.keymap.set(mode, l, r, opts)
-        end
-
-        map('n', ']c', function()
+        vim.keymap.set('n', ']c', function()
           if vim.wo.diff then
             vim.cmd.normal { ']c', bang = true }
           else
             gitsigns.nav_hunk 'next'
           end
-        end)
+        end, { buffer = bufnr })
 
-        map('n', '[c', function()
+        vim.keymap.set('n', '[c', function()
           if vim.wo.diff then
             vim.cmd.normal { '[c', bang = true }
           else
             gitsigns.nav_hunk 'prev'
           end
-        end)
+        end, { buffer = bufnr })
 
         vim.keymap.set('n', '<leader>hp', gitsigns.preview_hunk, { buffer = bufnr })
         vim.keymap.set('n', '<leader>hs', gitsigns.stage_hunk, { buffer = bufnr })
