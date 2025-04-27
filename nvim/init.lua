2cce4a00cb2c29ef7b7003594511006eec5e6318
prizelk <19prizel@gmail.com>
2025-04-26 18:58:03 -0700

    add highlight on yank

--- nvim/init.lua
+++ nvim/init.lua
@@ -353,13 +353,11 @@ end, {})
 
 -- [[ Highlight on yank ]]
 
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
+  group = vim.api.nvim_create_augroup('YankHighlight', { clear = true }),
   callback = function()
-    vim.highlight.on_yank()
+    vim.hl.on_yank()
   end,
-  group = highlight_group,
-  pattern = '*',
 })
 
 -- [[ Statusline ]]
