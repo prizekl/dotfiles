53ad1a4a43679c1e0744aef342583959ccc9511f
prizelk <19prizel@gmail.com>
2025-05-25 20:30:07 -0700

    zz

--- nvim/init.lua
+++ nvim/init.lua
@@ -28,15 +28,8 @@ vim.api.nvim_command 'packadd Cfilter'
 -- [[ Plugins ]]
 
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable',
-    lazypath,
-  }
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', 'https://github.com/folke/lazy.nvim.git', lazypath }
 end
 vim.opt.rtp:prepend(lazypath)
 
