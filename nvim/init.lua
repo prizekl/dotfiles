3292e14f216f4a8c96f6820a6431d102fa4af3b3
prizelk <19prizel@gmail.com>
2025-07-27 10:21:49 -0700

    rv

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,10 +8,6 @@
 
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-
 vim.wo.number = true
 vim.wo.signcolumn = 'yes'
 vim.o.clipboard = 'unnamedplus'
